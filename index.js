45df20dd7d4d83f74bf8dbf8a3b102c372cad992
caikangkang <caikangkang@migu.cn>
2020-08-21 15:58:11 +0800

    交互演示订阅发布模式

---
+++ index.js
@@ -0,0 +1,264 @@
+const noop = function () { console.log('i am noop') };
+
+const duration = 200;
+
+//如果传消息了 则取具体消息列表 否则取观察者列表
+const $selcetSuberWrapper = function (puber, msg) {
+  return !msg ? $(`.watcher-${puber.id}`).parent()
+    : $(`.puber-${puber.id}[data-msg='${msg}']`).parent()
+}
+//获取订阅者
+const $selectSuber = function (puber, msg, suber) {
+  return $(`.puber-${puber.id}[data-msg='${msg}']`).find(`.suber-${suber.id}`)
+}
+//获取观察者
+const $selectWatcher = function (puber, suber) {
+  return $(`.watcher-${puber.id}`).find(`.suber-${suber.id}`)
+}
+//闪动
+const $blink = function (selector) {
+  $(selector).fadeOut(duration, function(){
+    $(this).fadeIn(duration);
+  });
+}
+
+new Vue({
+  el: '#app',
+  data: function() {
+    return {
+      pubers: [],
+      subers: [],
+      curSuber: {},//要操作的订阅者
+      selectedPuber: {},//选中的发布者
+      subType: '',//操作类型 add:订阅 remove:解除
+      msgType: '',//消息类型 listen:订阅 publish:发布
+      msg: '',
+      msgIpt: ''
+    }
+  },
+  mounted() {
+    return false
+    for (var i = 0; i < 2; i++) {
+      if (i < 1) {
+        this.pubers.push(new Publish('pub' + i))
+      }
+      this.subers.push(new Subscribe('sub' + i))
+      this.doListen(this.subers[i], this.pubers[0], 'asdaflasdasdfs')
+      this.doListen(this.subers[i], this.pubers[0], '胜多负少')
+    }
+  },
+  methods: {
+    /**
+     * 创建发布者
+     */
+    addPuber() {
+      if (this.pubers.length > 1) {
+        alert('样式限制只能增加2个')
+        return false
+      }
+      let name = prompt("请输入发布者名称")
+      if (!name) return false
+      if (this.pubers.find(el => el.name == name)) {
+        alert("名字不能重复");
+        return false
+      }
+
+      let puber = new Publish(name);
+      this.pubers.push(puber)
+    },
+    /**
+     * 创建订阅者
+     */
+    addSuber() {
+      if (this.subers.length > 9) {
+        alert('只能增加10个')
+        return false
+      }
+      let name = prompt("请输入订阅者名称")
+      if (!name) return false
+      if (this.subers.find(el => el.name == name)) {
+        alert("名字不能重复");
+        return false
+      }
+
+      let suber = new Subscribe(name);
+      this.subers.push(suber)
+    },
+    /**
+     * 开始订阅
+     * @param {Subscribe} suber 订阅者
+     */
+    listen(suber) {
+      this.subType = 'add'; //订阅
+      this.msgType = 'listen'; //订阅
+      this.curSuber = suber;
+      this.selectedPuber = {};
+    },
+    /**
+     * 执行订阅
+     * @param {Subscribe} suber 订阅者
+     * @param {Publish} puber 发布者
+     * @param {String} msg 消息
+     * @param {Function} handler 回调函数
+     */
+    doListen(suber, puber, msg, handler = noop) {
+      if (Object.keys(puber.messageMap).length >= 10 && Object.keys(puber.messageMap).indexOf(msg) === -1) {
+        alert("不可超过10条消息")
+        return false
+      }
+      suber.listen({ publisher: puber, message: msg, handler });
+      this.closeMsgBox();
+      this.resetSuber();
+      this.$nextTick(() => {
+        $blink($selectSuber(puber, msg, suber))
+        $blink($selectWatcher(puber, suber))
+      })
+    },
+
+    /**
+     * 解除订阅
+     * @param {Subscribe} suber 订阅者
+     */
+    unlisten(suber) {
+      this.subType = 'remove'; //解除
+      this.msgType = '';
+      this.curSuber = suber;
+      this.selectedPuber = {};
+    },
+    /**
+     * 执行解除订阅
+     * @param {Subscribe} suber 订阅者
+     */
+    doUnlisten(suber, puber, msg) {
+      let type = 'fadeOut';
+      if (msg) {
+        //解除某一个消息下的订阅者
+        $selectSuber(puber, msg, suber)[type](duration, () => {
+          suber.unlisten(puber, msg);
+        })
+      } else {
+        //解除所有订阅者列表中的订阅者
+        for (let key in puber.messageMap) {
+          $selectSuber(puber, key, suber)[type](duration, () => {
+            suber.unlisten(puber, key);
+          })
+        }
+
+        //解除观察者列表中的观察者
+        $selectWatcher(puber, suber)[type](duration, () => {
+          suber.unlisten(puber, msg);
+        })
+      }
+
+      setTimeout(() => {
+        this.closeMsgBox();
+        this.resetSuber();
+      }, duration + 50)
+    },
+
+    /**
+     * 选择发布者
+     * @param {Publish} puber 发布者
+     */
+    choicePuber(puber) {
+      this.selectedPuber = puber;
+      this.toggleMsgBox(true)
+    },
+    /**
+     * 发布消息
+     * @param {Publish} puber 发布者
+     */
+    publish(puber) {
+      this.subType = '';
+      this.msgType = 'publish';
+      this.selectedPuber = puber;
+      this.toggleMsgBox(true);
+    },
+    /**
+     * 快速发布消息
+     * @param {Publish} puber 发布者
+     * @param {String} msg 消息
+     */
+    fastPublish(puber, msg) {
+      let value = prompt("请输入要发布的内容")
+      value !== null && this.doPublish(puber, msg, { value })
+    },
+    /**
+     * 执行发布消息
+     * @param {Publish} puber 发布者
+     * @param {String} msg 消息
+     * @param {Object} info 内容
+     */
+    doPublish(puber, msg, info) {
+      info = Object.assign({ _puberName: puber.name, _msgName: msg, _timePublish: Date.now() }, info)
+      
+      //若观察者列表有数据 就闪动表示收到消息
+      puber.watcherList.length && $blink($selcetSuberWrapper(puber))
+
+      msg && $blink($selcetSuberWrapper(puber, msg))
+      
+      puber.publish(msg, info);
+      this.closeMsgBox();
+      this.resetSuber();
+    },
+
+    /**
+     * 关闭选择
+     */
+    closeChoice() {
+      this.curSuber = {};
+      this.selectedPuber = {};
+    },
+    /**
+     * 关闭选择消息弹框
+     */
+    closeMsgBox() {
+      this.msg = '';
+      this.msgIpt = '';
+      this.toggleMsgBox(false)
+    },
+    /**
+     * 重置当前操作的订阅者
+     */
+    resetSuber() {
+      this.$nextTick(() => {
+        this.curSuber = {};
+      })
+    },
+    /**
+     * 
+     * @param {Boolean} type 方式
+     */
+    toggleMsgBox(type) {
+      if (type) {
+        $(".mask,.msg-box").fadeIn();
+      } else {
+        $(".mask,.msg-box").fadeOut();
+      }
+    },
+    /**
+     * 确认选择消息
+     */
+    msgConfirm() {
+      let msg = this.msgIpt || this.msg;
+
+      if (this.msgType === 'listen') { //订阅
+        let suberCopy = JSON.parse(JSON.stringify(this.curSuber));
+        let _timeListen = Date.now();
+        this.doListen(this.curSuber, this.selectedPuber, msg, function (info) {
+          console.log(`%c ${suberCopy.name} receive message:`, 'color:#fff;background:#03ff4b');
+          console.dir({ _timeListen, ...info })
+        })
+      }
+
+      if (this.msgType === 'publish') { //发布
+        let value = prompt("请输入要发布的内容")
+        this.doPublish(this.selectedPuber, msg, { value })
+      }
+
+      if (this.subType === 'remove') { //取订
+        this.doUnlisten(this.curSuber, this.selectedPuber, msg);
+      }
+    }
+  }
+})
---
+++ subpub.js
@@ -0,0 +1,131 @@
+//订阅者构造器
+class Subscribe {
+  constructor(name = 'subscriber') {
+    this.name = name
+    //随机id模拟唯一
+    this.id = Math.ceil(Math.random() * 10000) + 10000
+  }
+  /**
+   * 
+   * @param {Object} 
+   */
+  listen({
+    publisher,//订阅的是哪个发布者
+    message,//订阅的消息
+    handler//收到消息后的处理方法
+  }) {
+    //订阅消息的回调函数
+    this[message + "_handler"] = handler
+    publisher && publisher instanceof Publish && publisher.addListener(this, message)
+    return this
+  }
+  /**
+   * 
+   * @param {Publisher} publisher 发布者
+   * @param {String} message 消息
+   */
+  unlisten(publisher, message) {
+    publisher && publisher.removeListener(this, message)
+    return this
+  }
+}
+
+
+//发布者构造器
+class Publish {
+  constructor(name = 'publisher') {
+    this.messageMap = {} //消息事件订阅者集合对象
+    this.watcherList = [] //观察者列表
+    //随机id模拟唯一
+    this.id = Math.ceil(Math.random() * 10000) + 10000
+    this.name = name
+  }
+  /**
+   * 
+   * @param {Subscribe} subscriber 订阅者
+   * @param {String} message 消息
+   */
+  addListener(subscriber, message) { //添加消息订阅者
+    if (!subscriber) return false
+    const existInWatcherList = this.watcherList.findIndex(exitSubscriber => exitSubscriber.id === subscriber.id)
+    if (!message) { //若没有订阅任何消息 则认为是观察者模式
+      if (existInWatcherList === -1) {//观察者列表中不存在此订阅者
+        this.watcherList.push(subscriber)
+      } else {
+        this.watcherList[existInWatcherList] = subscriber;
+      }
+
+      //并取消这个订阅者已经订阅的消息
+      this.removeListener(subscriber, '', true)
+    } else { //订阅发布模式
+      //已经是观察者情况下 不用再单独订阅消息
+      if(existInWatcherList !== -1) return false
+
+      if (!this.messageMap[message]) { //如果消息列表不存在，就新建
+        this.messageMap[message] = []
+      }
+  
+      const existIndex = this.messageMap[message].findIndex(exitSubscriber => exitSubscriber.id === subscriber.id)
+      if (existIndex === -1) {//不存在这个订阅者时添加
+        this.messageMap[message].push(subscriber)
+      } else {//存在这个订阅者时更新回调handler
+        this.messageMap[message][existIndex][message + "_handler"] = subscriber[message + "_handler"]
+      }
+    }
+
+  }
+  /**
+   * 
+   * @param {Subscribe} subscriber 
+   * @param {String} message 
+   * @param {Boolean} self 内部转换为观察者模式时传入true
+   */
+  removeListener(subscriber, message, self = false) { //删除消息订阅者
+    if (!subscriber) return this
+
+    //如果传了message只删除此message下的订阅关系，否则删除此订阅者的所有订阅关系
+    const messageList = message ? [message] : Object.keys(this.messageMap)
+
+    messageList.forEach(message => {
+      const subscribers = this.messageMap[message];
+
+      if (!subscribers) return false;
+
+      let i = subscribers.length;
+      while (i--) {
+        if (subscribers[i].id === subscriber.id) {
+          subscribers.splice(i, 1)
+        }
+      }
+
+      //若是此消息没有订阅者了 删除这个消息列表
+      if (!subscribers.length) delete this.messageMap[message]
+    })
+
+    //观察者模式下删除订阅者
+    !message && !self && (this.watcherList = this.watcherList.filter(watcher => watcher.id !== subscriber.id))
+    return this
+  }
+  /**
+   * 
+   * @param {String} message 消息
+   * @param {Object} info 内容
+   */
+  publish(message, info) { //发布通知
+    //观察者模式 执行回调
+    this.watcherList.forEach(watcher => {
+      watcher._handler(info)
+    })
+
+    if (message) {//执行此消息下的订阅者回调
+      const subscribers = this.messageMap[message]
+      if (subscribers && subscribers.length) {
+        subscribers.forEach(subscriber => {
+          subscriber[message + "_handler"](info)
+        })
+      }
+    } 
+  
+    return this
+  }
+};
